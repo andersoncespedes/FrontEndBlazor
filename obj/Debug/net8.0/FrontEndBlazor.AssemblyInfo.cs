de91a90de869773709ac74f9e6d3157fb85bc2c7
anderson <andersontrabajos2@gmail.com>
2024-05-27 20:22:01 -0500

    feat: :bug: Probando soluciones

--- obj/Debug/net8.0/FrontEndBlazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/FrontEndBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEndBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c16b42964735e8e357cdf4fa430e53b2050001")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc4c13e75ccfe1a38ba70ad2964aef8e4b2721c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEndBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEndBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
